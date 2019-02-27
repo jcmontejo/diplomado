12daf7563f80152e6a188af9257914e4139365b7
jcmontejo <jncrlsmontejo@gmail.com>
2019-02-27 11:48:31 -0600

    cambio en avatar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -611,7 +611,7 @@ overflow-y: auto;
                     </div>
                     <div class="col-sm-6 clearfix">
                         <div class="user-profile pull-right">
-                            <img class="avatar user-thumb" src="{{asset('assets/images/author/avatar.png')}}" alt="avatar">
+                            <img class="avatar user-thumb" src="{{asset('/images/user.png')}}" alt="avatar">
                             <h4 class="user-name dropdown-toggle" data-toggle="dropdown"> {{ Auth::user()->name }} <i
                                     class="fa fa-angle-down"></i></h4>
                             <div class="dropdown-menu">
