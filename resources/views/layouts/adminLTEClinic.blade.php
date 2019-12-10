46ffced71257aa63ab10b64f73153d7054b6dc57
jcmontejo <jncrlsmontejo@gmail.com>
2019-12-10 17:30:41 -0600

    cambio de title en master

--- resources/views/layouts/adminLTEClinic.blade.php
+++ resources/views/layouts/adminLTEClinic.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>SERendipity | Control Escolar</title>
+    <title>SERendipity | Citas</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Tell the browser to be responsive to screen width -->
