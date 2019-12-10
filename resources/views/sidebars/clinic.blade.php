452210e7677d1f42253771e5b8b263328e5b05dd
jcmontejo <jncrlsmontejo@gmail.com>
2019-12-10 17:29:09 -0600

    arreglos en citas

--- resources/views/sidebars/clinic.blade.php
+++ resources/views/sidebars/clinic.blade.php
@@ -1,7 +1,7 @@
  <!-- Main Sidebar Container -->
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="{{url('/prueba/admin')}}" class="brand-link">
+        <a href="{{url('/clinica/inicio')}}" class="brand-link">
             <img src="{{asset('/adminLTE/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
                 class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">SERendipity</span>
@@ -27,7 +27,7 @@
                     <!-- Add icons to the links using the .nav-icon class
                     with font-awesome or any other icon font library -->
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
+                        <a href="{{url('/clinica/inicio')}}" class="nav-link">
                             <i class="nav-icon fas fa-home"></i>
                             <p>
                                 Inicio
